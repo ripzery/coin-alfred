3d8dbb87e57addcf5fb6d8567537fd4be4eb1e61
euro <phuchit@omg.network>
2020-12-12 22:08:44 +0700

    fix bugs

--- build/src/index.js
+++ build/src/index.js
@@ -1,20 +1,45 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.doSomeStuff = void 0;
-console.log('Try npm run lint/fix!');
-const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
-const trailing = 'Semicolon';
-const why = 'am I tabbed?';
-function doSomeStuff(withThis, andThat, andThose) {
-    //function on one line
-    if (!andThose.length) {
-        return false;
+const alfy = require('alfy');
+const axios_1 = require("axios");
+class Coingecko {
+    constructor() { }
+    async search(input) {
+        const coins = await this.fetch();
+        const criteria = (coin) => coin.name.toLowerCase().indexOf(input) > -1 ||
+            coin.symbol.indexOf(input) > -1;
+        const filteredCoins = coins.filter(criteria).slice(0, 5);
+        return this.getPrice(filteredCoins);
     }
-    console.log(withThis);
-    console.log(andThat);
-    console.dir(andThose);
-    return;
+    async getPrice(coins) {
+        const coinIds = coins.map(coin => coin.id).join('%2C');
+        const data = await axios_1.default
+            .get(`https://api.coingecko.com/api/v3/simple/price?ids=${coinIds}&vs_currencies=usd`)
+            .then(resp => resp.data)
+            .catch(err => alfy.output({ title: err.message }));
+        return coins.map(coin => ({ ...coin, price: data[coin.id].usd }));
+    }
+    async fetch() {
+        let data = alfy.cache.get('coins') || [];
+        if (data.length === 0) {
+            data = await axios_1.default
+                .get('https://api.coingecko.com/api/v3/coins/list')
+                .then(resp => resp.data);
+            alfy.cache.set('coins', data, { maxAge: 15000 });
+        }
+        return data;
+    }
 }
-exports.doSomeStuff = doSomeStuff;
-// TODO: more examples
+const coingecko = new Coingecko();
+async function run() {
+    const result = await coingecko.search(alfy.input).then(coins => coins.map((coin) => ({
+        title: coin.id,
+        subtitle: `${coin.price} USD`,
+        uid: coin.id,
+        match: coin.id,
+        autocomplete: coin.id,
+    })));
+    alfy.output(result);
+}
+run();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ class Coingecko {
   constructor() {}
 
   async search(input: string): Promise<CoinPrice[]> {
-    const coins = await this.fetch_price();
+    const coins = await this.fetch();
     const criteria = (coin: Coin) =>
       coin.name.toLowerCase().indexOf(input) > -1 ||
       coin.symbol.indexOf(input) > -1;
 
-    const filteredCoins = coins.filter(criteria);
+    const filteredCoins = coins.filter(criteria).slice(0, 5);
     return this.getPrice(filteredCoins);
   }
 
@@ -21,12 +21,13 @@ class Coingecko {
       .get(
         `https://api.coingecko.com/api/v3/simple/price?ids=${coinIds}&vs_currencies=usd`
       )
-      .then(resp => resp.data);
+      .then(resp => resp.data)
+      .catch(err => alfy.output({title: err.message}));
 
-    return coins.map(coin => ({...coin, price: data[coin.id].price}));
+    return coins.map(coin => ({...coin, price: data[coin.id].usd}));
   }
 
-  private async fetch_price(): Promise<Coin[]> {
+  private async fetch(): Promise<Coin[]> {
     let data: Coin[] = alfy.cache.get('coins') || [];
 
     if (data.length === 0) {
@@ -41,8 +42,9 @@ class Coingecko {
   }
 }
 
+const coingecko = new Coingecko();
+
 async function run() {
-  const coingecko = new Coingecko();
   const result = await coingecko.search(alfy.input).then(coins =>
     coins.map((coin: CoinPrice) => ({
       title: coin.id,
@@ -52,7 +54,6 @@ async function run() {
       autocomplete: coin.id,
     }))
   );
-  alfy.log(result);
   alfy.output(result);
 }
 
